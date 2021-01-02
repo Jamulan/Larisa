3789f36269884b06325b674cafe313d66a05daa6
jamulan <jamulan@ijuggle5.com>
2021-01-02 04:03:08 -0600

    Inital Commit

---
+++ Assets/SimpleCamaraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+
+public class SimpleCamaraController : MonoBehaviour
+{
+
+    class CameraState
+    {
+        public Vector3 orientation;
+        public Vector3 position;
+
+        public void SetFromTransform(Transform t)
+        {
+            orientation = t.eulerAngles;
+            position = t.position;
+        }
+
+        public void UpdateTransform(Transform t)
+        {
+            t.eulerAngles = orientation;
+            t.position = position;
+        }
+    }
+
+    CameraState cameraState = new CameraState();
+    
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        cameraState.SetFromTransform(transform);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Exit
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+        // Rotation
+        var mouseMovement = new Vector3(Input.GetAxis("Mouse Y") * -1, Input.GetAxis("Mouse X"), 0);
+
+        cameraState.orientation += mouseMovement;
+        cameraState.UpdateTransform(transform);
+    }
+}
