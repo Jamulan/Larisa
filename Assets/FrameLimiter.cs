348768c42c8c1f72663b0ea2866f43a03ce88bf2
jamulan <jamulan@ijuggle5.com>
2021-01-02 07:28:42 -0600

    Worked on some stuff mostly relating to limiting and monitoring frame times

---
+++ Assets/FrameLimiter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System;
+
+public class FrameLimiter : MonoBehaviour
+{
+    private FrameLimiter m_Instance;
+    public FrameLimiter Instance { get { return m_Instance; } }
+
+    public double FrameTimeLimit = 4.0f;
+
+    private long lastTime = DateTime.UtcNow.Ticks;
+
+    void Awake()
+    {
+        m_Instance = this;
+    }
+
+    void OnDestroy()
+    {
+        m_Instance = null;
+    }
+
+    void Update()
+    {
+        if (FrameTimeLimit == 0.0) return;
+
+        lastTime += TimeSpan.FromMilliseconds(FrameTimeLimit).Ticks;
+
+        var now = DateTime.UtcNow.Ticks;
+
+        if (now >= lastTime)
+        {
+            lastTime = now;
+            return;
+        }
+        else
+        {
+            SpinWait.SpinUntil(() => { return (DateTime.UtcNow.Ticks >= lastTime); });
+        }
+    }
+}
---
+++ Assets/PlayerStateManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerStateManager : MonoBehaviour
+{
+    public class PlayerState
+    {
+        public Vector3 pos;
+        public Vector3 look;
+
+        public void SetFromTransform(Transform t)
+        {
+            pos = t.position;
+            look = t.eulerAngles;
+        }
+
+        public void UpdateTransform(Transform t)
+        {
+            t.position = pos;
+            t.Find("Head").eulerAngles = look;
+            t.Find("Body").eulerAngles = new Vector3(0, look.y, 0);
+        }
+    }
+
+    private PlayerState playerState = new PlayerState();
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerState.SetFromTransform(transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+
+        var mouseMovement = new Vector3(Input.GetAxis("Mouse Y") * -1, Input.GetAxis("Mouse X"), 0);
+
+        playerState.look += mouseMovement;
+        
+        playerState.UpdateTransform(transform);
+    }
+}
